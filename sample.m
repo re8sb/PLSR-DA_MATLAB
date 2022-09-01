32e40071e435c4beacd35194755055e1bee769a4
re8sb <70545974+re8sb@users.noreply.github.com>
2022-09-01 10:13:14 -0400

    Provided sample code to run PLSR_main and PLSDA_main.

--- sample.m
+++ sample.m
@@ -1,20 +1,23 @@
-%% Sample call to PLSR and PLSDA.
+%% Sample call to PLSR and PLSDA. %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% RE 9/1/22
 close all
 clear; clc
 
-%% PLSR
+%% PLSR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 X = rand(10,10);
 Y = rand(10,1);
 varNames = {'a','b','c','d','e','f','g','h','i','j'}';
 nperm = 1000;
 ncomp = 2;
-model_PLSR = PLSR_main(X,Y,ncomp,varNames,'no','yes',{'kfold',5},nperm,'interactions');
+Y_data_name = 'output';
+model_PLSR = PLSR_main(X,Y,ncomp,varNames,'no','yes',{'kfold',5},nperm,Y_data_name);
 
-%% PLSDA
+%% PLSDA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 X = rand(10,10);
 Y = logical([1 1 1 1 1 0 0 0 0 0])'; Y = double([Y ~Y]);
 varNames = {'a','b','c','d','e','f','g','h','i','j'}';
 nperm = 1000;
 ncomp = 2;
+categories = {'group 1','group 2'};
 
-model_PLSDA = PLSDA_main(X,Y,ncomp,varNames,'no','yes',{'kfold',5},nperm,{'CD8','Macro'});
+model_PLSDA = PLSDA_main(X,Y,ncomp,varNames,'no','yes',{'kfold',5},nperm,categories);
